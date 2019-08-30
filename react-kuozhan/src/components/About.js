ad45f6286129f6097120292bd1b345c8d8684978
baseon2016 <baseon_000@163.com>
2019-08-30 17:18:04 +0800

    8-30 react-kuozhan 路由动画和懒加载

--- react-kuozhan/src/components/About.js
+++ react-kuozhan/src/components/About.js
@@ -4,7 +4,12 @@ class About extends Component {
   render() {
     return (
       <div style={{ width: "100vw", height: "60vh", backgroundColor: "green" }}>
-        About
+        <img
+          src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1567735542&di=944cfcd85a360974f0eea59c38c1f09f&imgtype=jpg&er=1&src=http%3A%2F%2Fpicm.bbzhi.com%2Fdongwubizhi%2Fxiaogoubizhi%2Fxiaogoubizhi_387320_m.jpg"
+          alt=""
+          style={{ width: "50vw" }}
+        />
+        <p>文字定位位置</p>
       </div>
     );
   }
--- react-kuozhan/src/components/Main.js
+++ react-kuozhan/src/components/Main.js
@@ -1,19 +1,30 @@
 import React, { Component } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, withRouter } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import Home from "./Home";
 import About from "./About";
+import "./main.css";
 class Main extends Component {
   state = {};
   render() {
+    const { location } = this.props;
     return (
-      <div>
-        <Switch>
-          <Route path="/" exact component={Home}></Route>
-          <Route path="/about" component={About}></Route>
-        </Switch>
-      </div>
+      //CSSTransition放在TransitionGroup触发动画的时候不能使用in 需要使用key属性
+      <TransitionGroup>
+        <CSSTransition
+          timeout={1000}
+          classNames="mainfade"
+          key={location.pathname}
+        >
+          {/* 路径变化前的Route 触发exit属性 变化后的Route 触发enter属性 */}
+          <Switch location={location}>
+            <Route path="/" exact component={Home}></Route>
+            <Route path="/about" component={About}></Route>
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
     );
   }
 }
 
-export default Main;
+export default withRouter(Main);
